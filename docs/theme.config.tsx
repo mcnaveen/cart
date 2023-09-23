76315b75361810855706bb54485b8408153e4d2c
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2023-09-23 22:04:58 +0530

    docs(docs): :memo: fix typo

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,7 +2,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import React from 'react'
 
 const config: DocsThemeConfig = {
-  docsRepositoryBase: 'https://github.com/mcnaveen/cart/tree/main/src/docs',
+  docsRepositoryBase: 'https://github.com/mcnaveen/cart/tree/main/docs',
   footer: {
     text: '$npm install cart',
   },
