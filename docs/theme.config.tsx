ce898454131602f1725eb7d1c330a229cdaf29ba
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2023-09-24 00:25:13 +0530

    chore(docs): :memo: add og tags

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import React from 'react'
 
@@ -10,6 +11,34 @@ const config: DocsThemeConfig = {
   project: {
     link: 'https://github.com/mcnaveen/cart',
   },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Cart'
+      }
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Cart - An Open Source headless cart management library" />
+      <meta property="og:description" content="$ npm install cart" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://cart.js.org/" />
+      <meta property="og:title" content="Cart - An Open Source headless cart management library" />
+      <meta property="og:description" content="$ npm install cart" />
+      <meta property="og:image" content="https://raw.githubusercontent.com/mcnaveen/cart/HEAD/image/cover.png" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://cart.js.org/" />
+      <meta property="twitter:title" content="Cart - An Open Source headless cart management library" />
+      <meta property="twitter:description" content="$ npm install cart" />
+      <meta property="twitter:image" content="https://raw.githubusercontent.com/mcnaveen/cart/HEAD/image/cover.png" />
+
+    </>
+  )
 }
 
 export default config
